1b9bcf02bc7646bd54025e6de85fd99244a8f7e5
Beo Học Ngu <100513440+BeoDepTraii@users.noreply.github.com>
2024-08-09 21:43:19 +0700

    fix

--- Du_an_1/App/Controllers/Admin/HomeController.php
+++ Du_an_1/App/Controllers/Admin/HomeController.php
@@ -17,29 +17,29 @@ class HomeController
     // hiển thị thống kê
     public static function index()
     {
-        // $user = new User();
-        // $total_user = $user -> countTotalUser();
+        $user = new User();
+        $total_user = $user -> countTotalUser();
 
-        // $category = new Category();
-        // $total_category = $category -> countTotalCategory();
+        $category = new Category();
+        $total_category = $category -> countTotalCategory();
 
-        // $product = new Product();
-        // $total_product = $product -> countTotalPrduct();
+        $product = new Product();
+        $total_product = $product -> countTotalPrduct();
 
-        // $comment = new Comment();
-        // $total_comment = $comment -> countTotalComment();
+        $comment = new Comment();
+        $total_comment = $comment -> countTotalComment();
 
-        // $product_by_category = $product->countProductByCaterogy();
+        $product_by_category = $product->countProductByCaterogy();
 
-        // $data =[
-        //     'total_user'=> $total_user['total'],
-        //     'total_category'=> $total_category['total'],
-        //     'total_product'=> $total_product['total'],
-        //     'product_by_category' => $product_by_category,
-        //     'total_comment' => $total_comment['total']
-        // ];
-        // // echo '<pre>';
-        // // var_dump($product_by_category);
+        $data =[
+            'total_user'=> $total_user['total'],
+            'total_category'=> $total_category['total'],
+            'total_product'=> $total_product['total'],
+            'product_by_category' => $product_by_category,
+            'total_comment' => $total_comment['total']
+        ];
+        // echo '<pre>';
+        // var_dump($product_by_category);
 
         Header::render();
         Home::render($data);
